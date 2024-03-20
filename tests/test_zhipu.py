cd3976659cf4a82ed458d0c4ffcf47a6dbfaec21
xiao-sq <864661641@qq.com>
2024-03-20 17:10:30 +0800

    fix

--- tests/test_zhipu.py
+++ tests/test_zhipu.py
@@ -3,22 +3,26 @@ import os
 import unittest
 
 # 将项目根目录添加到sys.path中
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from cnlitellm.utils import create_model_response, ModelResponse
 from cnlitellm.providers.zhipu import ZhipuAIProvider
 
+
 class TestZhipuAIProvider(unittest.TestCase):
     def setUp(self):
         # 请将'your_api_key'替换为您的Zhipu AI API密钥
-        self.provider = ZhipuAIProvider(api_key='your_api_key')
+        self.provider = ZhipuAIProvider(
+            api_key="e3833c6712b25eb4d89babd15c8134f5.Do904yhEYxDjewxI"
+        )
 
     def test_completion(self):
-        model = 'your_model_name'
-        messages = [{'content': '你好，今天天气怎么样？', 'role': 'user'}]
-        response = self.provider.completion(model='GLM-4', messages=messages)
-        model_response = create_model_response(response, model='GLM-4')
+        model = "your_model_name"
+        messages = [{"content": "你好，今天天气怎么样？", "role": "user"}]
+        response = self.provider.completion(model="GLM-4", messages=messages)
+        model_response = create_model_response(response, model="GLM-4")
         self.assertIsNotNone(model_response)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
