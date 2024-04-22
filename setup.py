d10ff8577b3abb4153746d95eecea7776df439be
quentinzhang <tagriver@gmail.com>
2024-04-22 17:56:05 +0800

    add supporting for Dify/FastGPT/Coze

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cnlitellm',
-    version='0.1',
+    version='0.2.0',
     packages=find_packages(),
     license='MIT',
     description='A Python library for unified access to Chinese domestic large language models.',
